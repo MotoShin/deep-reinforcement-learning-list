2fcc08aacdda7d2f475b4e8876cb46fa3ab421d4
MotoShin <m8o8t2o8@gmail.com>
2021-05-09 13:24:46 +0900

    lossの算出方法の変更

--- a2c/agent.py
+++ a2c/agent.py
@@ -132,7 +132,7 @@ class MasterAgent:
         actor_loss = (log_probs * advantage.detach()).mean()
         critic_loss = advantage.pow(2).mean()
 
-        loss = -1 * actor_loss + 0.5 * critic_loss + -1 * 0.01 * entropy
+        loss = actor_loss + 0.5 * critic_loss + -1 * 0.01 * entropy
         # print(loss.item())
 
         self.optimizer.zero_grad()
