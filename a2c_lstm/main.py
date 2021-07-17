a072d9cce6c72040ecb63596844c0a317f7c16ea
MotoShin <m8o8t2o8@gmail.com>
2021-07-17 10:58:17 +0900

    動くものができた

---
+++ a2c_lstm/main.py
@@ -0,0 +1,151 @@
+import os
+import sys
+import torch
+import time
+import ray
+import numpy as np
+from itertools import count
+
+sys.path.append(os.path.abspath(".."))
+from a2c_lstm.agent import Agent, MasterAgent, TestAgent
+from a2c_lstm.settings import *
+from common.data_util import DataShaping
+from common.line_notify import LineNotify
+from environment.cartpole import CartPole
+
+class Simulation(object):
+    def __init__(self):
+        self.reward = []
+        self.simulation_reward = []
+        self.ave_reward = []
+        self.simulation_ave_reward = []
+        self.record_param = {"loss": [], "actor_loss": [], "critic_loss": [], "entropy": []}
+        self.env = CartPole()
+        self.env.seed(SEED)
+        self.master_agent = MasterAgent(self.env.get_n_actions())
+        self.agent_name = "a2c"
+
+        ## https://stackoverflow.com/questions/54338013/parallel-import-a-python-file-from-sibling-folder
+        parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        os.environ["PYTHONPATH"] = parent_dir + ":" + os.environ.get("PYTHONPATH", "")
+
+    def simulation_reset(self):
+        self.master_agent = MasterAgent(self.env.get_n_actions())
+        self.reward = []
+        self.ave_reward = []
+    
+    def episode_reset(self):
+        self.env.reset()
+
+    def start(self):
+        start = time.time()
+        print('Start!')
+        for simulation_num in range(NUM_SIMULATION):
+            self.simulation_reset()
+            #### simulation start
+            self.one_simulation_start(simulation_num)
+            #### simulation end
+            for key, lst in zip(self.record_param.keys(), self.master_agent.get_record_param()):
+                self.record_param[key].append(lst)
+            self.simulation_reward.append(self.reward)
+            self.simulation_ave_reward.append(self.ave_reward)
+            if (simulation_num + 1) % 10 == 0:
+                DataShaping.makeCsv(self.simulation_reward, 'reward', "{}_reward_{}.csv".format(self.agent_name, simulation_num + 1))
+                DataShaping.makeCsv(self.simulation_ave_reward, 'reward', "{}_ave_reward_{}.csv".format(self.agent_name, simulation_num + 1))
+        DataShaping.makeCsv(self.simulation_reward, 'reward', "{}_reward.csv".format(self.agent_name))
+        DataShaping.makeCsv(self.simulation_ave_reward, 'reward', "{}_ave_reward.csv".format(self.agent_name))
+        for key in self.record_param.keys():
+            DataShaping.makeParamCsv(self.record_param[key], key, "{}_{}.csv".format(self.agent_name, key))
+        end = time.time()
+        LineNotify.send_line_notify(
+            LINE_NOTIFY_FLG,
+            LINE_NOTIFY_TOKEN,
+            LINE_NOTIFY_MSG.format(self._shape_time(end - start))
+        )
+        print('End!')
+
+    def one_simulation_start(self, simulation_num):
+        ray.init(local_mode=False)
+        agents = [Agent.remote(agent_id=i) for i in range(AGENTS_NUM)]
+
+        states = ray.get([agent.reset_env.remote() for agent in agents])
+        states = np.array(states)
+
+        for n in range(UPDATE_NUM):
+            self._output_progress(simulation_num, n)
+            for _ in range(TRAJECTORY_LENGTH):
+                actions = self.master_agent.select(torch.from_numpy(states).type(DTYPE) / 255.0)
+                states = ray.get([agent.step.remote(action.item()) for action, agent in zip(actions, agents)])
+                states = np.array(states)
+        
+            trajectories = ray.get([agent.get_collect_trajectory.remote() for agent in agents])
+
+            list_s2 = [trajectory["s2"][-1] for trajectory in trajectories]
+            values, _ = self.master_agent.get_netowrk_outputs(torch.from_numpy(np.array(list_s2)).type(DTYPE) / 255.0)
+
+            for trajectory, value in zip(trajectories, values):
+                if trajectory["dones"][-1] == 0:
+                    rewards = self._discounted_with_dones(trajectory["r"]+[value], trajectory["dones"]+[0])[:-1]
+                else:
+                    rewards = self._discounted_with_dones(trajectory["r"], trajectory["dones"])
+                trajectory["R"] = rewards
+
+            self.master_agent.learning(trajectories, n + 1)
+
+            if n % TEST_PLAY_TERM == 0:
+                self.test_play()
+                rewards = ray.get([agent.get_episode_reward.remote() for agent in agents])
+                self.ave_reward.append(float(sum(rewards)) / AGENTS_NUM)
+
+        for agent in agents:
+            agent.close.remote()
+        ray.shutdown()
+        self.env.close()
+
+    def test_play(self):
+        self.master_agent.save()
+        self.env.reset()
+        state = self.env.get_screen()
+        agent = TestAgent(self.env.get_n_actions())
+        
+        total_reward = 0
+        while True:
+            action = agent.select(state)
+            _, reward, done, _ = self.env.step(action)
+            state = self.env.get_screen()
+            total_reward += reward
+
+            if done:
+                break
+
+        self.reward.append(total_reward)
+
+    def _output_progress(self, simulation_num, update_num):
+        sim = simulation_num + 1
+        update = update_num + 1
+        late = float((simulation_num * UPDATE_NUM + update) / (NUM_SIMULATION * UPDATE_NUM))
+        late_percent = late * 100
+        if (late_percent % 10 == 0):
+            print("progress: {: >3} %".format(late_percent))
+
+    def _shape_time(self, elapsed_time):
+        elapsed_time = int(elapsed_time)
+
+        elapsed_hour = elapsed_time // 3600
+        elapsed_minute = (elapsed_time % 3600) // 60
+        elapsed_second = (elapsed_time % 3600 % 60)
+
+        return str(elapsed_hour).zfill(2) + ":" \
+                + str(elapsed_minute).zfill(2) + ":" \
+                + str(elapsed_second).zfill(2)
+
+    def _discounted_with_dones(self, rewards, dones):
+        discounted = []
+        r = 0
+        for reward, done in zip(rewards[::-1], dones[::-1]):
+            r = reward + GAMMA * r * (1. - done)
+            discounted.append(r)
+        return discounted[::-1]
+
+if __name__ == '__main__':
+    Simulation().start()
---
+++ a2c_lstm/network.py
@@ -0,0 +1,70 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from settings import *
+
+
+class ActorCriticNetwork(nn.Module):
+    def __init__(self, batch_size, outputs):
+        self.nhid = 300
+        self.batch_size = batch_size
+
+        super(ActorCriticNetwork, self).__init__()
+
+        # conv layer
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+
+        # common layer
+        self.fc4 = nn.Linear(7 * 7 * 64, 512)
+        
+        # actor layer
+        self.fc5_1 = nn.Linear(512, 128)
+        self.policy_logits = nn.Linear(128, outputs)
+        self.softmax = nn.Softmax(dim=1)
+
+        # critic layer
+        self.fc5_2 = nn.Linear(512, 128)
+        self.layerLSTM = nn.LSTMCell(128, self.nhid, bias=True)
+        self.hiddenLSTM = self._init_hidden(self.batch_size)
+        self._init_lstmCellWeight()
+        self.layerLinearPostLSTM = nn.Linear(self.nhid, 128)
+        self.values = nn.Linear(128, 1)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.fc4(x.view(x.size(0), -1)))
+
+        x1 = F.relu(self.fc5_1(x))
+        logits = self.policy_logits(x1)
+        action_probs = torch.distributions.categorical.Categorical(self.softmax(logits))
+
+        x2 = F.relu(self.fc5_2(x))
+        self.hiddenLSTM = self.layerLSTM(x2, self.hiddenLSTM)
+        x2 = F.relu(self.layerLinearPostLSTM(self.hiddenLSTM[0]))
+        values = self.values(x2)
+
+        return values, action_probs
+
+    def learning(self, x):
+        self.hiddenLSTM = self._init_hidden(AGENTS_NUM * TRAJECTORY_LENGTH)
+        values, action_probs = self.forward(x)
+        self.hiddenLSTM = self._init_hidden(self.batch_size)
+        return values, action_probs
+
+    def _init_hidden(self, bsz):
+        weight = next(self.parameters())
+        return (weight.new_zeros(bsz, 300),
+                weight.new_zeros(bsz, 300))
+
+    def _init_lstmCellWeight(self):
+        # the xavier initialization here is different than the one at the original code of critic_cartpole.
+		# taken from: https://discuss.pytorch.org/t/initializing-parameters-of-a-multi-layer-lstm/5791
+        for name, param in self.layerLSTM.named_parameters():
+            if 'bias' in name:
+                nn.init.constant_(param, 1.0)
+            elif 'weight' in name:
+                nn.init.xavier_normal_(param)
